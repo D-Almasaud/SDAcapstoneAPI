21501ce79bf40498d722a38a407944b0153c2a58
rand1199 <rba.tmimi@gmil.com>
2024-05-20 16:09:23 +0300

    user services

--- src/test/java/Test/Rand/UserServices.java
+++ src/test/java/Test/Rand/UserServices.java
@@ -1,15 +1,19 @@
 package Test.Rand;
 
 import baseUrl.QuasparepartsBaseUrl;
+import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
+import pojos.UserServices.CreateUserPOJO;
+import utilities.ObjectMapperUtils;
 
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
 
 public class UserServices extends QuasparepartsBaseUrl {
-
+    CreateUserPOJO requestBody;
+    int id;
     @Test
     public void GetOrSearchUsersOfApplication() {
         spec.pathParams("first","v1"
@@ -30,7 +34,7 @@ public class UserServices extends QuasparepartsBaseUrl {
     public void GetOrSearchUsersOfOrganization() {
         spec.pathParams("first","v1"
                 ,"second","organization"
-                ,"third","1715291982442324"
+                ,"third","1716152660399179"
                 ,"forth","user");
 
         Response response = given(spec)
@@ -46,9 +50,9 @@ public class UserServices extends QuasparepartsBaseUrl {
     public void DeleteUsersOfOrganization() {
         spec.pathParams("first", "v1",
                 "second", "organization",
-                "third", "11715291982442324",
+                "third", "1716152660399179",
                 "forth", "user",
-                "fifth", 1813);
+                "fifth", 40);
 
         Response response = given(spec)
                 .when()
@@ -68,14 +72,24 @@ public class UserServices extends QuasparepartsBaseUrl {
                 "forth", "register-manual");
 
         // Set the request body for the POST request
-        String requestBody = "{\n" +
-                "    \"app_id\": 2,\n" +
-                "    \"organization_id\": 1715291772802536,\n" +
-                "    \"email\": \"rm.506@hotmail.com\",\n" +
-                "    \"subscription_id\": \"05b57412-412a-4f10-9877-a9c9734f2159\",\n" +
-                "    \"default_role_id\": 23\n" +
-                "}";
+        String requestBodyStr = """
+                {
+                    "id": null,
+                    "app_id": 2,
+                    "organization_id": 1716152660399179,
+                    "email": "assurew.tmimi@gmail.com",
+                    "subscription_id": "af7db802-90cc-46d8-b256-d4bf1d0f735d",
+                    "default_role_id": 19,
+                    "user_groups": [
+                        {
+                            "id": 28
+                        }
+                    ]
+                }""";
 
+        requestBody = ObjectMapperUtils.convertJsonToJava(requestBodyStr, CreateUserPOJO.class);
+        requestBody.setEmail(Faker.instance().internet().emailAddress());
+        requestBody.setId(null);
         Response response = RestAssured.given()
                 .spec(spec)
                 .body(requestBody) // Set the request body
@@ -84,9 +98,12 @@ public class UserServices extends QuasparepartsBaseUrl {
 
         // Print response body
         response.prettyPrint();
-
+        CreateUserPOJO actualDAta = ObjectMapperUtils.convertJsonToJava(response.asString(), CreateUserPOJO.class);
+        id = actualDAta.getId();
         // Assert response status code is 200
         assertEquals(response.statusCode(), 201);
+        assertEquals(actualDAta.getEmail(),requestBody.getEmail());
+        assertEquals(actualDAta.getOrganization_id(),requestBody.getOrganization_id());
     }
 
 
@@ -104,40 +121,13 @@ public class UserServices extends QuasparepartsBaseUrl {
         assertEquals(response.statusCode(), 200);
     }
 
-    @Test
+    @Test(dependsOnMethods = "addRegisterNewUserManually")
     public void updateRegisterNewUserManually() {
         spec.pathParams("first", "v1", "second", "user");
 
         // Set the request body for the PUT request
-        String requestBody = "{\n" +
-                "  \"id\": 0,\n" +
-                "  \"name\": \"John\",\n" +
-                "  \"lastname\": \"JAMES\",\n" +
-                "  \"username\": \"john\",\n" +
-                "  \"email\": \"ra@example.com\",\n" +
-                "  \"is_email_verified\": true,\n" +
-                "  \"phone\": \"+1 123 123 4567\",\n" +
-                "  \"address\": \"string\",\n" +
-                "  \"country_id\": \"US\",\n" +
-                "  \"pic_id\": 12345,\n" +
-                "  \"preferences\": {},\n" +
-                "  \"status_id\": 12345,\n" +
-                "  \"user_groups\": [\n" +
-                "    {\n" +
-                "      \"id\": 0,\n" +
-                "      \"name\": \"Sales Department\",\n" +
-                "      \"short_name\": \"Sales Dept.\",\n" +
-                "      \"group_type_id\": 0,\n" +
-                "      \"pic_id\": 12345,\n" +
-                "      \"organization_id\": 12345,\n" +
-                "      \"is_head\": false\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"created_at\": \"2024-05-16T06:41:26.883Z\",\n" +
-                "  \"created_by\": 0,\n" +
-                "  \"updated_at\": \"2024-05-16T06:41:26.883Z\",\n" +
-                "  \"updated_by\": 0\n" +
-                "}";
+        requestBody.setEmail("some@thing.known");
+        requestBody.setId(id);
 
         Response response = RestAssured.given()
                 .spec(spec)
@@ -147,9 +137,11 @@ public class UserServices extends QuasparepartsBaseUrl {
 
         // Print response body
         response.prettyPrint();
+        CreateUserPOJO actualData = ObjectMapperUtils.convertJsonToJava(response.asString(),CreateUserPOJO.class);
 
         // Assert response status code is 200
         assertEquals(response.statusCode(), 200);
+        assertEquals(actualData.getEmail(),requestBody.getEmail());
     }
 
 
@@ -172,7 +164,7 @@ public class UserServices extends QuasparepartsBaseUrl {
     public void GetUserById() {
         spec.pathParams("first","v1"
                 ,"second","user"
-                ,"third",1715);
+                ,"third",39);
 
         Response response = given(spec)
                 .when()
@@ -187,7 +179,7 @@ public class UserServices extends QuasparepartsBaseUrl {
     public void DeleteExistingUserById() {
         spec.pathParams("first", "v1",
                 "second", "user",
-                "third", "1821");
+                "third", "28");
 
         Response response = given(spec)
                 .when()
