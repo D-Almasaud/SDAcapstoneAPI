3139721a39a34bea3bb7705232e56c653b5af0ce
ReemaEbbie <reema.ebbie@gmail.com>
2024-05-20 16:37:15 +0300

    last updte

--- src/test/java/pojos/organization/CreateOrgnization.java
+++ src/test/java/pojos/organization/CreateOrgnization.java
@@ -4,7 +4,7 @@ import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
 import utilities.ObjectMapperUtils;
 
-import static baseUrl.QuadripartiteBaseUrl.spec;
+import static baseUrl.QuasparepartsBaseUrl.spec;
 
 public class CreateOrgnization {
 
